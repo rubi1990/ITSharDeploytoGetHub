baa54c615ebacfd95e1c16b2f8d9f4fd36ebc8f3
DESKTOP-OHPU02E\Daniel <rubaawad1995@gmail.com>
2021-06-30 11:19:49 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
+// this is test
 if (environment.production) {
   enableProdMode();
 }
