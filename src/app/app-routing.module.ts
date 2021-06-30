89682b3cd52b507637ee022efc93d860b481c2d0
DESKTOP-OHPU02E\Daniel <rubaawad1995@gmail.com>
2021-06-30 11:07:03 +0200

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { LoginComponent } from './login/login.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+
+const routes: Routes = [
+  {path: 'products',component:ProductsComponent},
+  {path: 'orders',component:MyOrdersComponent},
+  {path: 'admin/orders',component:AdminOrdersComponent},
+  {path: 'admin/products',component:AdminProductsComponent},
+  {path: 'shopping-cart',component:ShoppingCartComponent},
+  {path: 'login',component:LoginComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
